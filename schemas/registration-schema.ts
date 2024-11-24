11f0f4d94c28ea3c7a77ef42238677344ff44218
Agamya Samuel <github@agamya.dev>
2024-11-24 22:32:52 +0530

    added course

--- schemas/registration-schema.ts
+++ schemas/registration-schema.ts
@@ -9,9 +9,20 @@ export const courseOptions = [
 	{ label: 'BCA 1st Year', value: 'bca_1st_year' },
 	{ label: 'BCA 2nd Year', value: 'bca_2nd_year' },
 	{ label: 'BCA 3rd Year', value: 'bca_3rd_year' },
-	{ label: 'BSc 3rd Year', value: 'bsc_3rd_year' },
 	{ label: 'MCA 1st Year', value: 'mca_1st_year' },
 	{ label: 'MCA 2nd Year', value: 'mca_2nd_year' },
+	{ label: 'BSc 1st Year', value: 'bsc_1st_year' },
+	{ label: 'BSc 2nd Year', value: 'bsc_2nd_year' },
+	{ label: 'BSc 3rd Year', value: 'bsc_3rd_year' },
+	{ label: 'BTech ECE 1st Year', value: 'btech_ece_1st_year' },
+	{ label: 'BTech ECE 2nd Year', value: 'btech_ece_2nd_year' },
+	{ label: 'BTech ECE 3rd Year', value: 'btech_ece_3rd_year' },
+	{ label: 'BTech ECE 4th Year', value: 'btech_ece_4th_year' },
+	{ label: 'BTech EE 1st Year', value: 'btech_ee_1st_year' },
+	{ label: 'BTech EE 2nd Year', value: 'btech_ee_2nd_year' },
+	{ label: 'BTech EE 3rd Year', value: 'btech_ee_3rd_year' },
+	{ label: 'BTech EE 4th Year', value: 'btech_ee_4th_year' },
+	{ label: 'Other', value: 'other' },
 ];
 
 export const moneyPaidOptions = [{ label: '₹500', value: '500' }];
